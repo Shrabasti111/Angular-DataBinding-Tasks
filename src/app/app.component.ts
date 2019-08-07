372830828651248a42d5b225f13e69cd3ddb2dbc
shrabro <shrabro1@in.ibm.com>
2019-08-07 20:29:51 +0530

    Scenario 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Scenario1';
+
+  displayName(nameEntered) {
+    console.log(nameEntered);
+  }
+}
